783ea815107a228db180943b76e205672b0af2e3
Deepika Gupta <deepikagupta8050@gmail.com>
2026-07-30 22:54:21 +0530

    Add 1047_maximize_sum_of_array_after_k_negations.cpp - LeetCode Problem 1047

---
+++ 1047_maximize_sum_of_array_after_k_negations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (nums[i] < 0 && k > 0) {
+                nums[i] = -nums[i];
+                k--;
+            }
+        }
+
+        sort(nums.begin(), nums.end());
+
+        if (k % 2 == 1) {
+            nums[0] = -nums[0];
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            ans += nums[i];
+        }
+
+        return ans;
+    }
+};
