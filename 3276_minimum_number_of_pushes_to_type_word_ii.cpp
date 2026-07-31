1c095e6c38ef2a399f5d4c828d86804faf2e65fd
Deepika Gupta <deepikagupta8050@gmail.com>
2026-07-31 20:13:54 +0530

    Add 3276_minimum_number_of_pushes_to_type_word_ii.cpp - LeetCode Problem 3276

---
+++ 3276_minimum_number_of_pushes_to_type_word_ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+
+        vector<int> count(26, 0);
+
+        for (int i = 0; i < word.size(); i++) {
+            count[word[i] - 'a']++;
+        }
+
+        sort(count.begin(), count.end(), greater<int>());
+
+        int ans = 0;
+
+        for (int i = 0; i < 26; i++) {
+
+            if (count[i] == 0) {
+                break;
+            }
+
+            ans += count[i] * (i / 8 + 1);
+        }
+
+        return ans;
+    }
+};
