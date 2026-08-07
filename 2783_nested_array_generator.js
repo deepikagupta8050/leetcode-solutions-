a850b49dd1826dc5e635b7d5b5e035d880cf8b2c
Deepika Gupta <deepikagupta8050@gmail.com>
2026-08-07 19:59:16 +0530

    Add 2783_nested_array_generator.js - LeetCode Problem 2783

---
+++ 2783_nested_array_generator.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function* (arr) {
+
+    for (let item of arr) {
+
+        if (Array.isArray(item)) {
+            yield* inorderTraversal(item);
+        } else {
+            yield item;
+        }
+    }
+
+};
