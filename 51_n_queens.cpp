b2cfb5170af14ebb4e5cdbc36c66e8c56810c04f
Deepika Gupta <deepikagupta8050@gmail.com>
2026-08-10 23:56:34 +0530

    Add 51_n_queens.cpp - LeetCode Problem 51

---
+++ 51_n_queens.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+
+    bool safe(vector<string>& board, int row, int col, int n) {
+
+        // Same column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void solve(vector<string>& board, int row, int n) {
+
+        if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+
+            if (safe(board, row, col, n)) {
+
+                board[row][col] = 'Q';
+
+                solve(board, row + 1, n);
+
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+
+        vector<string> board(n, string(n, '.'));
+
+        solve(board, 0, n);
+
+        return ans;
+    }
+};
