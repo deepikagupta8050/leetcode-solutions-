53e2b3d70bc4804283020888eaa334d1e79c23dc
Deepika Gupta <deepikagupta8050@gmail.com>
2026-08-15 23:27:25 +0530

    Add 493_reverse_pairs.cpp - LeetCode Problem 493

---
+++ 493_reverse_pairs.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    long long mergeSort(vector<int>& nums, int left, int right) {
+        if (left >= right) {
+            return 0;
+        }
+
+        int mid = left + (right - left) / 2;
+        long long count = 0;
+
+        count += mergeSort(nums, left, mid);
+        count += mergeSort(nums, mid + 1, right);
+
+        int j = mid + 1;
+
+        for (int i = left; i <= mid; i++) {
+            while (j <= right && (long long)nums[i] > 2LL * nums[j]) {
+                j++;
+            }
+            count += j - (mid + 1);
+        }
+
+        vector<int> temp;
+        int i = left;
+        j = mid + 1;
+
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                temp.push_back(nums[i++]);
+            } else {
+                temp.push_back(nums[j++]);
+            }
+        }
+
+        while (i <= mid) {
+            temp.push_back(nums[i++]);
+        }
+
+        while (j <= right) {
+            temp.push_back(nums[j++]);
+        }
+
+        for (int i = 0; i < temp.size(); i++) {
+            nums[left + i] = temp[i];
+        }
+
+        return count;
+    }
+
+    int reversePairs(vector<int>& nums) {
+        return (int)mergeSort(nums, 0, nums.size() - 1);
+    }
+};
