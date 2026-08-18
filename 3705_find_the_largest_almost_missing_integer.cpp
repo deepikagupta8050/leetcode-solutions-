1e81cc18daebac3d587db4d76a8ca4c933b1ae24
Deepika Gupta <deepikagupta8050@gmail.com>
2026-08-18 22:28:08 +0530

    Add 3705_find_the_largest_almost_missing_integer.cpp - LeetCode Problem 3705

---
+++ 3705_find_the_largest_almost_missing_integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ans = -1;
+
+        for (int x = 0; x <= 50; x++) {
+            int count = 0;
+
+            for (int i = 0; i + k <= n; i++) {
+                for (int j = i; j < i + k; j++) {
+                    if (nums[j] == x) {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            if (count == 1) {
+                ans = x;
+            }
+        }
+
+        return ans;
+    }
+};
